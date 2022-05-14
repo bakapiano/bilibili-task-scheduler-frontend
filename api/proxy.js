57e2134bc273869ffd0ac4078ec0619cc44ca603
bakapiano <bakapiano@outlook.com>
2022-05-14 23:13:55 +0800

    Edit vercal config files

--- api/proxy.js
+++ api/proxy.js
@@ -3,7 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
   let target = ''
 
-  if (req.url.startsWith('/api')) {
+  // 代理目标地址
+  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
+  if (req.url.startsWith('/backend')) {
     target = 'http://121.4.44.73:8000'
   }
 
@@ -12,7 +14,9 @@ module.exports = (req, res) => {
     target,
     changeOrigin: true,
     pathRewrite: {
-      '^/api/': '/'
+      // 通过路径重写，去除请求路径中的 `/backend`
+      // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
+      '^/backend/': '/'
     }
   })(req, res)
 }
