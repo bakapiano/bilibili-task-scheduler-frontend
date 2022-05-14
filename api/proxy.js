fc231de081256bc50023036c2bc579e8a8cc641b
bakapiano <bakapiano@outlook.com>
2022-05-14 23:26:05 +0800

    Edit vercal function

--- api/proxy.js
+++ api/proxy.js
@@ -1,22 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
-  let target = ''
-
-  // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  if (req.url.startsWith('/backend')) {
-    target = 'http://121.4.44.73:8000'
-  }
-
-  // 创建代理对象并转发请求
   createProxyMiddleware({
-    target,
+    target: "http://121.4.44.73:8000",
     changeOrigin: true,
-    pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/backend`
-      // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-      '^/backend/': '/'
-    }
   })(req, res)
 }
